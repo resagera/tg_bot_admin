a9849da8f32020bea8a36a801882a83c768f96c1
resager <resagera@gmail.com>
2026-03-14 01:15:13 +0400

    Added commands

--- main.go
+++ main.go
@@ -224,15 +224,20 @@ func (s *Store) ListPages(userID int64) []string {
 }
 
 var allowedExecutables = map[string]bool{
-	"echo":   true,
-	"date":   true,
-	"uname":  true,
-	"uptime": true,
-	"whoami": true,
-	"pwd":    true,
-	"ls":     true,
-	"cat":    true,
-	"grep":   true,
+	"echo":    true,
+	"date":    true,
+	"uname":   true,
+	"uptime":  true,
+	"whoami":  true,
+	"pwd":     true,
+	"ls":      true,
+	"cat":     true,
+	"grep":    true,
+	"history": true,
+	"df":      true,
+	"dh":      true,
+	"du":      true,
+	"jq":      true,
 }
 
 func main() {
